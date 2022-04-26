b2fd4d56449e7da61b3053ea17692eb00f1536c7
ecloud <projetgroup383@gmail.com>
2022-04-26 11:54:13 +0200

    server

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -2,4 +2,11 @@ package com.example;
 
 public class User {
     public String description;
+    public String nom;
+    public String prenom;
+
+
+    public User (String nom, String prenom,String description){
+
+    }
 }
