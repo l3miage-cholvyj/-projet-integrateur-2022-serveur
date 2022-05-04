753a4fa5b374bee4e1ac10765ca4b56aab8f6592
ProjetIntegrateurEquipe7-2022 <projetgroup383@gmail.com>
2022-05-04 13:42:32 +0200

    reorganisation

---
+++ src/main/java/com/example/repository/LesDefisRepository.java
@@ -0,0 +1,9 @@
+
+package com.example.Controller;
+
+import com.example.model.LesDefis;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LesDefisRepository extends JpaRepository<LesDefis,String>{
+}
